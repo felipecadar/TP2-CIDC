aa64b89e6ec4b755a3cee1cee873383de67587a3
Felipe Cadar <cadar@dcc.ufmg.br>
2025-01-10 20:28:28 -0300

    update pv and pvc

--- redeploy_back.sh
+++ redeploy_back.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker build -t radac98/playlist-frontend:latest ./playlist-recommendation-system/frontend                                                
-docker push radac98/playlist-frontend:latest
+docker build -t radac98/playlist-frontend:1.0.0 ./playlist-recommendation-system/frontend                                                
+docker push radac98/playlist-frontend:1.0.0
 
-docker build -t radac98/playlist-backend:latest ./playlist-recommendation-system/backend
-docker push radac98/playlist-backend:latest
+docker build -t radac98/playlist-backend:1.0.0 ./playlist-recommendation-system/backend
+docker push radac98/playlist-backend:1.0.0
 
-docker build -t radac98/playlist-ml-training ./playlist-recommendation-system/ml-training
-docker push radac98/playlist-ml-training:latest
+docker build -t radac98/playlist-ml-training:1.0.0 ./playlist-recommendation-system/ml-training
+docker push radac98/playlist-ml-training:1.0.0
