6832ce15f0288b1a3dfab64bc3703d7c0740f931
Felipe Cadar <cadar@dcc.ufmg.br>
2025-01-10 21:46:39 -0300

    update deploy

--- redeploy_back.sh
+++ redeploy_back.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker build -t radac98/playlist-frontend:1.0.0 ./playlist-recommendation-system/frontend                                                
-docker push radac98/playlist-frontend:1.0.0
+docker build -t quay.io/radac98/playlist-frontend:1.0.0 ./playlist-recommendation-system/frontend                                                
+docker push quay.io/radac98/playlist-frontend:1.0.0
 
-docker build -t radac98/playlist-backend:1.0.0 ./playlist-recommendation-system/backend
-docker push radac98/playlist-backend:1.0.0
+docker build -t quay.io/radac98/playlist-backend:1.0.0 ./playlist-recommendation-system/backend
+docker push quay.io/radac98/playlist-backend:1.0.0
 
-docker build -t radac98/playlist-ml-training:1.0.0 ./playlist-recommendation-system/ml-training
-docker push radac98/playlist-ml-training:1.0.0
+docker build -t quay.io/radac98/playlist-ml-training:1.0.0 ./playlist-recommendation-system/ml-training
+docker push quay.io/radac98/playlist-ml-training:1.0.0
